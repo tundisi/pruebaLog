3042cd3f865ae23d0851200a9e3d78e5f19cfa78
luistundisi <luistundisi@gmail.com>
2017-08-08 12:19:11 -0400

    Prueba

---
+++ src/main/java/com/nextdots/retargetly/retargetly/LogDebug.java
@@ -0,0 +1,14 @@
+package com.nextdots.retargetly.retargetly;
+
+import android.util.Log;
+
+/**
+ * Created by luistundisi on 8/8/17.
+ */
+
+public class LogDebug {
+
+    static public void hola(){
+        Log.d("--","Besame");
+    }
+}
