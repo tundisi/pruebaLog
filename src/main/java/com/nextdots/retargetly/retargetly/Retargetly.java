cf7f51edebc41c153809ed9c7eae586a35d3dd60
luistundisi <luistundisi@gmail.com>
2017-08-09 10:38:48 -0400

    probando actividades

---
+++ src/main/java/com/nextdots/retargetly/retargetly/Retargetly.java
@@ -0,0 +1,56 @@
+package com.nextdots.retargetly.retargetly;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by luistundisi on 8/8/17.
+ */
+
+public class Retargetly {
+
+    private Activity activeActivity;
+    Application application;
+
+    public void inicialize(Application application){
+        this.application = application;
+        setupActivityListener();
+    }
+
+    private void setupActivityListener() {
+        application.registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+            }
+            @Override
+            public void onActivityStarted(Activity activity) {
+                Log.d("--LIBRARY Start",""+activity.getPackageName());
+            }
+            @Override
+            public void onActivityResumed(Activity activity) {
+                Log.d("--LIBRARY Resumed",""+activity.getPackageName());
+                activeActivity = activity;
+            }
+            @Override
+            public void onActivityPaused(Activity activity) {
+                activeActivity = null;
+            }
+            @Override
+            public void onActivityStopped(Activity activity) {
+            }
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+            }
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+            }
+        });
+    }
+
+    public Activity getActiveActivity(){
+        return activeActivity;
+    }
+
+}
